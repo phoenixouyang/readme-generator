3dc84b759a234c3d33cd88516ce3d41d2c5c3307
Pholobo <ouyangphoenix@gmail.com>
2023-02-26 13:27:10 -0500

    adds array of questions to prompt user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,57 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        section: "title",
+        message: "[required] Enter your project title:",
+    },
+    {
+        type: "input",
+        section: "Description",
+        message: "[required] Enter a short description of your project:",
+    },
+    {
+        type: "input",
+        section: "Installation",
+        message: "[required] Are there any steps required to install your project? List them here:",
+    },
+    {
+        type: "input",
+        section: "Usage",
+        message: "[required] Enter instructions for the use of your application. Feel free to include examples as well:",
+    },
+    {
+        type: "input",
+        section: "Contributing",
+        message: "[required] List any contributors for your application, and provide instructions on how others can contribute:",
+    },
+    {
+        type: "input",
+        section: "Tests",
+        message: "Describe any tests that were performed for this application:",
+    },
+    {
+        type: "list",
+        section: "License",
+        message: "Choose the license used for your application:",
+        choices: ["Apache", "GNU General Public License, MIT", "BSD 2-Clause", "BSD 3-Clause", "Boost", "Creative Commons", "Eclipse", "GNU Affero", "GNU General", "GNU Lesser", "Mozilla", "Unilicense",]
+    },
+    {
+        type: "input",
+        section: "username",
+        message: "Please enter your Github username:",
+    },
+    {
+        type: "input",
+        section: "email",
+        message: "Please enter the email where people can reach you for questions about your application:"
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
---
+++ index.js
@@ -0,0 +1,22 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    `Please enter your project title:`,
+    `Enter a short description of your project:`,
+    `Are there any installation steps required for your project?:`,
+    `Enter instructions for use of your application:`,
+    `Enter a list of any collaborators for this project:`,
+    `Put information about your license below:`,
+    `Put a list of your features below:`,
+    `Type instructions to contribute to your project:`,
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
